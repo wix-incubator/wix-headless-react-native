fda9c639fa6cf6ede43587708fe17fe0269ccd3b
Ronen Sivak <140695222+RonenSivak@users.noreply.github.com>
2024-02-26 16:16:55 +0200

    added real collection to home screen

--- components/ShopCollectionsHome/ShopCollectionsHome.js
+++ components/ShopCollectionsHome/ShopCollectionsHome.js
@@ -1,29 +1,35 @@
 import {Dimensions, Image, Pressable, Text, TouchableOpacity, View} from "react-native";
 import {styles} from "../../styles/home/shop-collection/shop-collection";
 import {ImageCard} from "../Cards/ImageCard";
-import {collections} from "../../data/homeScreen/ShopCollection/data";
 import Carousel from 'react-native-reanimated-carousel';
 import {useWixModules} from "@wix/sdk-react";
-import {products} from "@wix/stores";
+import {collections, products} from "@wix/stores";
 import {useQuery} from "@tanstack/react-query";
 import {IconButton} from "react-native-paper";
 import {useRef} from "react";
 import {LoadingIndicator} from "../LoadingIndicator/LoadingIndicator";
 import {ErrorView} from "../ErrorView/ErrorView";
+import {WixMediaImage} from "../../WixMediaImage";
+
+const screenWidth = Dimensions.get('window').width;
 
 export const ShopCollectionsHome = ({navigation}) => {
-    const width = Dimensions.get('window').width;
     const {queryProducts} = useWixModules(products);
+    const {queryCollections} = useWixModules(collections);
     const carouselRef = useRef(null);
-
     const productsResponse = useQuery(["products"], () => queryProducts().find());
+    const collectionsResponse = useQuery(["collections"], () => queryCollections().find());
 
-    if (productsResponse.isLoading) {
+    if (productsResponse.isLoading || collectionsResponse.isLoading) {
         return <LoadingIndicator/>
     }
 
     if (productsResponse.isError) {
         return <ErrorView message={productsResponse.error.message}/>
+    }
+
+    if (collectionsResponse.isError) {
+        return <ErrorView message={collectionsResponse.error.message}/>
     }
 
     const handleNext = () => {
@@ -32,6 +38,11 @@ export const ShopCollectionsHome = ({navigation}) => {
     const handlePrev = () => {
         carouselRef.current?.prev();
     }
+    const shopCollections = [...collectionsResponse.data.items];
+    const firstThreeCollections = shopCollections.slice(0, 3).map(collection =>
+        collection.name.toLowerCase() === "all products" ? shopCollections[3] : collection
+    );
+
     const newProducts = [...productsResponse.data.items];
     const filteredProducts = newProducts.filter((product) => product.collectionIds.includes('50ce695b-b834-0414-48a8-45dcf7cba52d'));
     return (
@@ -39,16 +50,35 @@ export const ShopCollectionsHome = ({navigation}) => {
             <Text style={styles.title}>Shop Collections</Text>
             <View style={styles.collections}>
                 {
-                    collections.map((collection, index) => {
+                    firstThreeCollections.map((item, index) => {
                         return (
                             <View key={index} style={{
                                 marginVertical: 10,
                             }}>
-                                <ImageCard imageSrc={{
-                                    uri: collection.uri,
-                                }}
-                                           title={collection.title}
-                                />
+                                {item.media?.mainMedia?.image?.url ? (
+                                    <WixMediaImage
+                                        media={item.media.mainMedia.image.url}
+                                        width={screenWidth * 0.8}
+                                        height={screenWidth * 0.8}
+                                    >
+                                        {({url}) => {
+                                            return (
+                                                <ImageCard imageSrc={{
+                                                    uri: url,
+                                                }}
+                                                           title={item.name}
+                                                />
+                                            );
+                                        }}
+                                    </WixMediaImage>) : (
+                                    <ImageCard
+                                        source={{
+                                            uri: `https://via.placeholder.com/${screenWidth / 2}`
+                                        }}
+                                        title={item.name}
+
+                                    />)
+                                }
                             </View>
                         );
                     })
@@ -63,8 +93,8 @@ export const ShopCollectionsHome = ({navigation}) => {
                         position: 'relative',
                     }}
                     loop
-                    width={width * 0.6}
-                    height={width}
+                    width={screenWidth * 0.6}
+                    height={screenWidth}
                     autoPlay={false}
                     data={filteredProducts}
                     scrollAnimationDuration={1000}
