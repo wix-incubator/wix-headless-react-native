532662f0438212f83430eccfb049ab5192ca55c4
Ronen Sivak <140695222+RonenSivak@users.noreply.github.com>
2024-03-06 16:57:06 +0200

    fixed hero section height

--- components/Hero/HeroSection.js
+++ components/Hero/HeroSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Dimensions, Image, Pressable, StatusBar, Text, View} from 'react-native';
 import {styles} from '../../styles/home/hero/styles';
 import Routes from "../../routes/routes";
+import {WixMediaImage} from "../../WixMediaImage";
 
 const windowHeight = Dimensions.get('window').height;
 
@@ -15,17 +16,26 @@ export const HeroSection = ({navigation}) => {
               onLayout={(event) => {
                   const {y} = event.nativeEvent.layout;
                   const bottomTabHeight = windowHeight * 0.1;
-                  setHeight(y + bottomTabHeight - StatusBar.currentHeight);
+                  setHeight(y + bottomTabHeight + (-StatusBar.currentHeight || 59));
               }}
         >
             <View style={styles.heroImageContainer}>
-                <Image
-                    source={{
-                        uri:
-                            'https://static.wixstatic.com/media/c837a6_938269b105e04d2c8a3462e43263ecf4~mv2.jpg/v1/fill/w_1441,h_982,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c837a6_938269b105e04d2c8a3462e43263ecf4~mv2.jpg',
+                <WixMediaImage
+                    media="https://static.wixstatic.com/media/c837a6_938269b105e04d2c8a3462e43263ecf4~mv2.jpg"
+                    width={1441}
+                    height={982}
+                >
+                    {({url}) => {
+                        return (
+                            <Image
+                                source={{
+                                    uri: url,
+                                }}
+                                style={styles.heroImage}
+                            />
+                        );
                     }}
-                    style={styles.heroImage}
-                />
+                </WixMediaImage>
                 <View style={styles.overlay}></View>
             </View>
             <View style={styles.heroText}>
